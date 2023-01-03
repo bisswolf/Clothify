dc003f30722ec1bcbe829c007a079a1e4309ab5e
bisswolf <divyaanshu2000@gmail.com>
2023-01-03 15:39:43 +0530

    commit

--- server/index.js
+++ server/index.js
@@ -3,10 +3,9 @@ const db = require("./db");
 const cors = require("cors");
 const dotenv = require("dotenv");
 const bodyParser = require("body-parser");
-const cloudinary = require("cloudinary").v2;
 const productRoutes = require("./routes/productRoutes");
 const userRoutes = require("./routes/userRoutes");
-
+const path = require("path");
 dotenv.config();
 // const clothModel = require("./model/clothModel");
 // const userModel = require("./model/userModel");
@@ -15,14 +14,13 @@ const port = 5000;
 
 app.use(cors());
 app.use(express.json());
+app.use(express.static(path.join(__dirname, "build")));
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use("/", productRoutes);
 app.use("/", userRoutes);
 
-cloudinary.config({
-  cloud_name: process.env.CLOUDINARY_NAME,
-  api_key: process.env.CLOUDINARY_API_KEY,
-  api_secret: process.env.CLOUDINARY_API_SECRET,
+app.get("/*", function (req, res) {
+  res.sendFile(path.join(__dirname, "build", "index.html"));
 });
 
 app.listen(port, () => console.log(`listening on port ${port}!`));
